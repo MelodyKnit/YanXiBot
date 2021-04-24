ba3ca4c85b8a5356f6dcde1c197147fe0d813a57
2711402357@qq.com <yanximelody@gmail.com>
2021-04-24 13:13:58 +0800

    更新机器人数据库插件

--- src/plugins/botdb/__init__.py
+++ src/plugins/botdb/__init__.py
@@ -1,14 +1,48 @@
-from nonebot import get_driver
+from .data_source import MySQLdb
+from pymysql.err import IntegrityError
 
-from .config import Config
 
-global_config = get_driver().config
-config = Config(**global_config.dict())
+class DBMethods(MySQLdb):
+    """
+    集成在数据库的一些方法
+    """
+    def create_user_info_table(self):
+        sql = (f"CREATE TABLE IF NOT EXISTS {self.INFO_TABLE_NAME}("
+               "qid BIGINT PRIMARY KEY COMMENT '用户QQid',"
+               "first_group BIGINT NOT NULL COMMENT '第一次注册所在群',"
+               "nickname VARCHAR(16) COMMENT '用户名称',"
+               "create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT '创建时间'"
+               ")")
+        self.execute(sql)
 
-# Export something for other plugin
-# export = nonebot.export()
-# export.foo = "bar"
+    def insert(self, query: str, args=None):
+        try:
+            self.execute(f"insert into {query}", args)
+            self.database.commit()
+        except IntegrityError:
+            self.database.rollback()
 
-# @export.xxx
-# def some_function():
-#     pass
+    def select(self, query: str, args=None):
+        self.execute(f"select {query}", args)
+        return self.cursor.fetchall()
+
+# print(os.getcwd())
+# pprint(os.getenv("PATH"))
+
+
+class BotMethods(DBMethods):
+    """
+    机器人操作数据库的方法
+    """
+    def add_user_init_info(self, qid: int, gid: int):
+        self.insert(f"{self.INFO_TABLE_NAME} "
+                    f"(qid,first_group) values "
+                    f"(%s,%s)", [qid, gid])
+
+
+class BotDB(BotMethods):
+    __slots__ = ()
+
+    def init(self):
+        self.create_user_info_table()
+
