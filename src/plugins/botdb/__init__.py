8ac08a245acc63a3496dcad1513d4cfd16c53764
Melodyknit <2711402357@qq.com>
2021-04-24 15:42:51 +0800

    reset

--- src/plugins/botdb/__init__.py
+++ src/plugins/botdb/__init__.py
@@ -1,33 +1,4 @@
-from .data_source import MySQLdb
-from pymysql.err import IntegrityError
-
-
-class DBMethods(MySQLdb):
-    """
-    集成在数据库的一些方法
-    """
-    def create_user_info_table(self):
-        sql = (f"CREATE TABLE IF NOT EXISTS {self.INFO_TABLE_NAME}("
-               "qid BIGINT PRIMARY KEY COMMENT '用户QQid',"
-               "first_group BIGINT NOT NULL COMMENT '第一次注册所在群',"
-               "nickname VARCHAR(16) COMMENT '用户名称',"
-               "create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT '创建时间'"
-               ")")
-        self.execute(sql)
-
-    def insert(self, query: str, args=None):
-        try:
-            self.execute(f"insert into {query}", args)
-            self.database.commit()
-        except IntegrityError:
-            self.database.rollback()
-
-    def select(self, query: str, args=None):
-        self.execute(f"select {query}", args)
-        return self.cursor.fetchall()
-
-# print(os.getcwd())
-# pprint(os.getenv("PATH"))
+from .data_source import DBMethods
 
 
 class BotMethods(DBMethods):
--- src/plugins/botdb/data_source.py
+++ src/plugins/botdb/data_source.py
@@ -1,7 +1,10 @@
 import pymysql
 from json import loads
+from os.path import exists
 from pymysql.err import IntegrityError
 
+
+CONFIG_DIR = "__config__\\" if exists("__config__") else "config\\"
 INFO_TABLE_NAME = "user_info"
 
 
@@ -14,7 +17,7 @@ def read_config_file(path: str) -> dict:
 
 
 def config(path: str, kwargs: dict) -> dict:
-    return read_config_file(path or "mysql.json") | kwargs
+    return read_config_file(path or f"{CONFIG_DIR}mysql.json") | kwargs
 
 
 class MySQLdb:
@@ -30,3 +33,28 @@ class MySQLdb:
     def __del__(self):
         self.database.close()
         self.cursor.close()
+
+
+class DBMethods(MySQLdb):
+    """
+    集成在数据库的一些方法
+    """
+    def create_user_info_table(self):
+        sql = (f"CREATE TABLE IF NOT EXISTS {self.INFO_TABLE_NAME}("
+               "qid BIGINT PRIMARY KEY COMMENT '用户QQid',"
+               "first_group BIGINT NOT NULL COMMENT '第一次注册所在群',"
+               "nickname VARCHAR(16) COMMENT '用户名称',"
+               "create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT '创建时间'"
+               ")")
+        self.execute(sql)
+
+    def insert(self, query: str, args=None):
+        try:
+            self.execute(f"insert into {query}", args)
+            self.database.commit()
+        except IntegrityError:
+            self.database.rollback()
+
+    def select(self, query: str, args=None):
+        self.execute(f"select {query}", args)
+        return self.cursor.fetchall()
