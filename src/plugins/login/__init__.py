097cfe4a466a6cf577d8ad74ac015c140f8e6e77
Melodyknit <2711402357@qq.com>
2021-04-25 17:51:28 +0800

    添加签到插件

--- src/plugins/login/__init__.py
+++ src/plugins/login/__init__.py
@@ -1,14 +1,19 @@
 from nonebot.rule import T_State
 from nonebot.adapters.cqhttp import Bot, GroupMessageEvent
 from nonebot import on_command
-from ...src.plugins.botdb import get_bot_db
+from botdb import get_bot_db
 
 
 bot_db = get_bot_db()
+# bot_db.init()
 
 login = on_command("签到")
 
 
 @login.handle()
 async def login_handle(bot: Bot, event: GroupMessageEvent, state: T_State):
-    print(event.get_plaintext())
+    if not bot_db.select_user(event.user_id):
+        bot_db.add_user(event.user_id, event.group_id)
+        await login.finish("签到成功")
+    else:
+        await login.finish("已经签到过了")
